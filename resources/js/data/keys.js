f489bc6a4193fadc021b859f37b04931f8edcd3b
Bob Makhlin <borismakhlin5@gmail.com>
2020-08-20 16:02:53 +0300

    Create footer

---
+++ resources/js/data/keys.js
@@ -0,0 +1,3 @@
+const mapboxKey = 'pk.eyJ1IjoiYm9ibWFraGxpbiIsImEiOiJja2Uxcjkwd28wMjZpMnNvYms2cnB3cjcxIn0.nlHkfPeqSxneDKSjXaenjg';
+
+export { mapboxKey };
--- resources/js/my-index.js
+++ resources/js/my-index.js
@@ -1,15 +1,20 @@
 import Drink from './drawers/drink.js';
 import { fakeDrinksData } from './data/fake-data.js';
 import Heart from './drawers/heart.js';
+import { mapboxKey } from './data/keys.js';
 
 const drinks = document.querySelector('.drinks');
+const companyLocation = document.querySelector('#company-location');
 
-init();
+let map = null;
 
 
+initDrinks();
+initMap();
 
-function init() {
-    for(let drinkInfo of fakeDrinksData) {
+
+function initDrinks() {
+    for (let drinkInfo of fakeDrinksData) {
         let drinkDrawer = new Drink(drinkInfo.image, drinkInfo.name, drinkInfo.price, drinkInfo.description);
         let heartDrawer = new Heart();
 
@@ -19,4 +24,14 @@ function init() {
 
         drinks.append(drinkDiv);
     }
+}
+function initMap() {
+    map = L.map('company-location').setView([40.745350, -74.012438], 10.5);
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: mapboxKey
+    }).addTo(map);
 }
