fc6afe26a602e52644bf79a69917080a61695c12
Bob Makhlin <borismakhlin5@gmail.com>
2020-08-25 13:34:28 +0300

    Show drinks in catalog page

--- resources/js/catalog.js
+++ resources/js/catalog.js
@@ -1,5 +1,24 @@
 import initMobileMenu from './initializers/mobile-menu.js';
 import initMap from './initializers/map.js';
+import { loadJson } from './helpers/loadingHelper.js';
+import { cafeDrinksUrl, cafeImagesUrl } from './data/urls.js';
+import Drink from './drawers/drink.js';
+
+const drinksContainer = document.querySelector('.drinks');
+
 
 initMobileMenu();
 const map = initMap('company-location');
+showDrinks();
+
+
+async function showDrinks() {
+    let drinks = await loadJson(cafeDrinksUrl);
+
+    for (let drink of drinks) {
+        let imageUrl = `${cafeImagesUrl}/${drink.image}`;
+
+        let drinkDrawer = new Drink(imageUrl, drink.name, 'Try coffees from Keniya, Ethiopia', drink.prices);
+        drinksContainer.append(drinkDrawer.toHtmlElement());
+    }
+}
