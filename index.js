e1b7192210b742c18e667430d2459ace1e96d001
Bob Makhlin <borismakhlin5@gmail.com>
2020-08-28 10:37:08 +0300

    Add the routing

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const express = require('express');
+const expressHbs = require('express-handlebars');
 const router = require('./routes/routes.js');
 
 const PORT = process.env.PORT || 3000;
-const app = express();
 
+
+const app = express();
+const hbs = expressHbs.create({
+    extname: 'hbs',
+    helpers: {
+        section: function (name, options) {
+            if (!this._sections) this._sections = {};
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    }
+});
 
 app.use(router);
-configeServer();
+configureServer();
 startServer();
 
 
-function configeServer() {
+function configureServer() {
+    app.use(express.static('public'));
+
+    app.engine('.hbs', hbs.engine);
+    app.set("view engine", "hbs");
 }
 function startServer() {
     app.listen(PORT, () => {
--- routes/routes.js
+++ routes/routes.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Main page');
+    res.render('index.hbs');
 });
 router.get('/catalog', (req, res) => {
-    res.send('Catalog page');
+    res.render('catalog.hbs');
 })
 router.get('/contacts', (req, res) => {
     res.send('Contancts page');
